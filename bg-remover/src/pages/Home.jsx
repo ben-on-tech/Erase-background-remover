22e6b4ce4ae9d89db64b01f2d48bcef84e1461d1
Beniam Lemma <benonforever@gmail.com>
2025-06-29 21:20:44 +0300

    created the Bg_slider.jsx

--- bg-remover/src/pages/Home.jsx
+++ bg-remover/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/steps'
+import Bg_slider from '../components/Bg_slider'
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <Steps/>
+      <Bg_slider/>
     </div>
   )
 }
