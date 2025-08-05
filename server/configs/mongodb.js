ff39bfaf48bb7bf69291e9556665d3bfaf63f4af
Beniam Lemma <benonforever@gmail.com>
2025-08-05 14:08:06 +0300

    fixed mongoDB.js

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose"
 
 const connectDB = async ()=>{
-    mongoose.connection.on('connected',()=>{
-        console.log('Database Connected')
-    })
-    await mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connection.on('connected',()=>{
+        console.log('Database Connected')
+    })
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'bg-removal'
+    })
 }
 
 export default connectDB
